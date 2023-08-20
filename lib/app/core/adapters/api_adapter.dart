d68e589134b2ea83e9794deb6ce9f1bbd27d5c48
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-08-20 12:33:00 -0300

    Adicionado adaptador para arquivos json

---
+++ lib/app/core/adapters/api_adapter.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/services.dart';
+
+import '../../constants.dart';
+
+abstract class ApiAdapter {
+  Future readAll();
+}
+
+class ApiAdapterImpl implements ApiAdapter {
+  final String fileName;
+
+  ApiAdapterImpl({required this.fileName});
+
+  @override
+  Future<String> readAll() async {
+    return await rootBundle.loadString("$jsonPath$fileName");
+  }
+}
